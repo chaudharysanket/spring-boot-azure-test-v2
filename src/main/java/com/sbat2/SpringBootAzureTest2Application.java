d7d022122736147f4aa9653ffa5195651b253528
Sanket Chaudhary <65975945+chaudharysanket@users.noreply.github.com>
2023-04-09 19:58:34 +0530

    Add files via upload

---
+++ src/main/java/com/sbat2/SpringBootAzureTest2Application.java
@@ -0,0 +1,13 @@
+package com.sbat2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAzureTest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAzureTest2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sbat2/SpringBootAzureTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbat2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAzureTest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
